68da2663e2a177d2c4ee4f8f98d1845b5c613212
naufalafif76 <naufalafif766@gmail.com>
2022-06-16 09:12:04 +0700

    [edit] app.js & routes/users.js

---
+++ routes/products.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+const middlewares = require('../middlewares');
+const products = require('../controllers/products');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+router.get('/all', middlewares.authorization.authorize, products.product.getProduct);
+router.get('/:id', middlewares.authorization.authorize, products.product.getProductbyId)
+router.get('/user/:id', middlewares.authorization.authorize, products.product.getMyProduct);
+router.get('/user/:user/:id', middlewares.authorization.authorize, products.product.getMyProductbyId);
+
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const middlewares = require('../middlewares');
+const users = require('../controllers/users');
+const products = require('../controllers/products');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+router.post('/register', middlewares.registerRules.checkCondition, users.register.register);
+router.get('/data/:id', middlewares.authorization.authorize, users.profile.getUserId);
+router.put('/data/:id', middlewares.authorization.authorize, users.profile.editUserId);
 
 module.exports = router;
