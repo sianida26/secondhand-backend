44f491b946212f87bce0f7c925e6109a2b38fbf3
“serwilhan” <serwilhan@gmail.com>
2022-06-16 14:31:23 +0800

    feat: add login router

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var router = express.Router();
 const middlewares = require('../middlewares');
+
 const users = require('../controllers/users');
-const products = require('../controllers/products');
+// const products = require('../controllers/products');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
+
+router.post('/login', users.loginController.handleLogin);
 router.post('/register', middlewares.registerRules.checkCondition, users.register.register);
+
 router.get('/data/:id', middlewares.authorization.authorize, users.profile.getUserId);
 router.put('/data/:id', middlewares.authorization.authorize, users.profile.editUserId);
 
