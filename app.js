06648de77815964ecd504503449e3d93e04a6e62
naufalafif76 <naufalafif766@gmail.com>
2022-06-17 21:19:51 +0700

    [edit] app.js & users.js

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+require('dotenv').config();
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
 
+const swaggerDocument = require('./docs/swagger.json');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var productsRouter = require('./routes/products');
@@ -10,6 +14,7 @@ var productsRouter = require('./routes/products');
 var app = express();
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -18,5 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/products', productsRouter);
+
+app.get('/documentation.json', (req, res) => res.send(swaggerDocument));
+app.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,22 @@
 var express = require('express');
 var router = express.Router();
+// const fs = require('fs');
+// const path = require('path');
+// const multer = require('multer');
+// const storage = multer.diskStorage({
+//   destination: '',
+//   filename: ''
+// });
 const middlewares = require('../middlewares');
 const users = require('../controllers/users');
-const products = require('../controllers/products');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
 router.post('/register', middlewares.registerRules.checkCondition, users.register.register);
-router.get('/data/:id', middlewares.authorization.authorize, users.profile.getUserId);
 router.put('/lengkapi-profil', middlewares.authorization.authorize, users.profile.editUserId);
+// router.get('/data/:id', middlewares.authorization.authorize, users.profile.getUserId);
 
 module.exports = router;
