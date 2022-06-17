2aef46688162510d47fe26271ea66a255e296945
serwilhan <serwilhan@gmail.com>
2022-06-17 23:07:44 +0800

    update database.js

--- config/database.js
+++ config/database.js
@@ -1,29 +1,30 @@
-const { DB_USERNAME = 'postgres', DB_PASSWORD = 'root', DB_NAME = 'db_secondhand', DB_HOST = 'localhost', DB_PORT = '5432', DB_DIALECT = 'postgres' } = process.env;
+// const { DB_USERNAME = 'postgres', DB_PASSWORD = 'root', DB_NAME = 'db_secondhand', DB_HOST = 'localhost', DB_PORT = '5432', DB_DIALECT = 'postgres' } = process.env;
+require('dotenv').config();
 
 module.exports = {
   development: {
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: DB_NAME,
-    host: DB_HOST,
-    port: DB_PORT,
-    dialect: DB_DIALECT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
   },
   test: {
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: DB_NAME,
-    host: DB_HOST,
-    port: DB_PORT,
-    dialect: DB_DIALECT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
   },
   production: {
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: DB_NAME,
-    host: DB_HOST,
-    port: DB_PORT,
-    dialect: DB_DIALECT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
     ssl: true,
     dialectOptions: {
       ssl: {
