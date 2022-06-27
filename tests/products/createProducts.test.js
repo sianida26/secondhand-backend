96ba8ed365357da36f706792f02964cb2d213afa
serwilhan <serwilhan@gmail.com>
2022-06-27 20:57:01 +0800

    test: create createProduct & editProductById test

--- tests/products/createProducts.test.js
+++ tests/products/createProducts.test.js
@@ -1,33 +1,27 @@
 const request = require('supertest');
+
 const app = require('../../app');
-const multer = require('multer');
+const path = require('path');
 
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwibmFtZSI6InVzZXIyIiwiZW1haWwiOiJ1c2VyMkBtYWlsLmNvbSIsImlhdCI6MTY1NTczNzIzMX0.eMPu2PlTQNDNmWaFxSmXybcGtg5ekK6NlaQSwdjVdVc';
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, './images/products');
-  },
-  filename: (res, file, cb) => {
-    cb(null, file.fieldname + '-' + Date.now() + '-' + file.originalname);
-  },
-});
-
-const upload = multer({ storage: storage });
-const cpUpload = upload.fields([{ name: 'filenames', maxCount: 4 }]);
+const accessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6InVzZXIxIiwiZW1haWwiOiJ1c2VyMUBtYWlsLmNvbSIsImlhdCI6MTY1NjI5NDAwM30.aJWiyCbfcgAJqH5C9lRG8fcF7IBKGfarlZIviD59_II';
 
 describe('POST /products', () => {
-  it('should response with 200 as status code and return success message', async () => {
+  it('should response with 200 and return success message', async () => {
     const name = 'product65';
     const price = 900000;
     const category = 'product';
     const description = 'product desc';
+    const filenames = path.resolve(__dirname, './img-test/test.jpg');
 
     return request(app)
       .post('/products')
       .set('Content-Type', 'multipart/form-data')
-      .set('Authorization', `Bearer ${token}`)
-      .send({ name, price, category, description })
+      .set('Authorization', `Bearer ${accessToken}`)
+      .field('name', name)
+      .field('price', price)
+      .field('category', category)
+      .field('description', description)
+      .attach('filenames', filenames)
       .then((res) => {
         expect(res.statusCode).toBe(200);
         expect(res.body).toEqual(
@@ -38,27 +32,63 @@ describe('POST /products', () => {
       });
   });
 
-  it('should response with 422 as status code and return message for required input', async () => {
+  it('should response with 422 and return message for required input', async () => {
     const name = 'product65';
     const price = 900000;
     const category = 'product';
-    // const description = 'product desc';
-    // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwibmFtZSI6InVzZXIyIiwiZW1haWwiOiJ1c2VyMkBtYWlsLmNvbSIsImlhdCI6MTY1NTczNzIzMX0.eMPu2PlTQNDNmWaFxSmXybcGtg5ekK6NlaQSwdjVdVc';
+    const filenames = path.resolve(__dirname, './img-test/test.jpg');
 
-    return (
-      request(app)
-        .post('/products')
-        .set('Content-Type', 'application/json')
-        // .setHeader('Authorization', `Bearer ${token}`)
-        .send({ name, price, category })
-        .then((res) => {
-          expect(res.statusCode).toBe(422);
-          expect(res.body).toEqual(
-            expect.objectContaining({
-              message: 'Semua input harus diisi',
-            })
-          );
-        })
-    );
+    return request(app)
+      .post('/products')
+      .set('Content-Type', 'application/json')
+      .set('Authorization', `Bearer ${accessToken}`)
+      .field('name', name)
+      .field('price', price)
+      .field('category', category)
+      .attach('filenames', filenames)
+      .then((res) => {
+        expect(res.statusCode).toBe(422);
+        expect(res.body).toEqual(
+          expect.objectContaining({
+            message: 'Semua input harus diisi',
+          })
+        );
+      });
+  });
+
+  it('should response with 422 and return max file message', async () => {
+    const name = 'product65';
+    const price = 900000;
+    const category = 'product';
+    const description = 'product desc';
+    const testFilenames = {
+      img: path.resolve(__dirname, './img-test/test.jpg'),
+      img2: path.resolve(__dirname, './img-test/test2.jpg'),
+      img3: path.resolve(__dirname, './img-test/test3.jpg'),
+      img4: path.resolve(__dirname, './img-test/test4.jpg'),
+      img5: path.resolve(__dirname, './img-test/test5.jpg'),
+    };
+
+    return request(app)
+      .post('/products')
+      .set('Content-Type', 'application/json')
+      .set('Authorization', `Bearer ${accessToken}`)
+      .field('name', name)
+      .field('price', price)
+      .field('category', category)
+      .field('description', description)
+      .attach('filenames', testFilenames.img)
+      .attach('filenames', testFilenames.img2)
+      .attach('filenames', testFilenames.img3)
+      .attach('filenames', testFilenames.img4)
+      .attach('filenames', testFilenames.img5)
+      .then((res) => {
+        expect(res.statusCode).toBe(422);
+        expect(res.body).toEqual(
+          expect.objectContaining({
+            message: 'File maximal 4',
+          })
+        );
+      });
   });
 });
