51028d7f65a09fc406d2bd91b91829135f7d339d
naufalafif76 <naufalafif766@gmail.com>
2022-06-28 18:27:31 +0700

    [add] bidController.js

--- controllers/bids/bidController.js
+++ controllers/bids/bidController.js
@@ -41,6 +41,138 @@ module.exports = {
         errors: err.message
       });
     }
+  },
+
+  async handleGetNotifications(req, res) {
+    try {
+      const userId = req.user;
+      const myProducts = await Products.findAndCountAll({
+        where: { createdBy: userId.id },
+        include: ['bids']
+      });
+      const myBids = await Bids.findAndCountAll({
+        where: { buyerId: userId.id },
+        include: ['products']
+      });
+
+      // myProducts.rows.sort((a, b) => {
+      //   if (a.bids != '' && b.bids != '') {
+      //     return new Date(b.bids.updatedAt).getTime() - new Date(a.bids.updatedAt).getTime()
+      //   }
+      //   if (a.bids != '') {
+      //     return new Date(b.updatedAt).getTime() - new Date(a.bids.updatedAt).getTime()
+      //   }
+      //   if (b.bids != '') {
+      //     return new Date(b.bids.updatedAt).getTime() - new Date(a.updatedAt).getTime()
+      //   }
+      //   return new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime()
+      // });
+
+      let id = 0;
+      let notif = [];
+
+      myProducts.rows.map((product) => {
+        let images = product.filenames ? JSON.parse(product.filenames).map(image => `https://secondhand-backend-kita.herokuapp.com/images/products/${image}`)[0] : "";
+
+        notif.push({
+          id: 0,
+          productName: product.name,
+          price: product.price,
+          image: images,
+          type: "Berhasil diterbitkan",
+          time: product.updatedAt
+        });
+
+        if (product.bids != '') {
+          product.bids.map((bid) => {
+            if (bid.bidPrice) {
+              // && !bid.declinedAt && !bid.soldAt
+              notif.push({
+                id: 0,
+                productName: product.name,
+                price: product.price,
+                image: images,
+                type: "Penawaran Produk",
+                bidPrice: bid.bidPrice,
+                time: bid.createdAt
+              });
+            }
+
+            if (bid.declinedAt) {
+              notif.push({
+                id: 0,
+                productName: product.name,
+                price: product.price,
+                image: images,
+                type: "Penawaran ditolak",
+                bidPrice: bid.bidPrice,
+                time: bid.declinedAt
+              });
+            }
+
+            if (bid.soldAt) {
+              notif.push({
+                id: 0,
+                productName: product.name,
+                price: product.price,
+                image: images,
+                type: "Berhasil Terjual",
+                bidPrice: bid.bidPrice,
+                time: bid.soldAt
+              });
+            }
+          });
+        }
+
+      });
+
+      myBids.rows.map((bid) => {
+        let images = bid.products.filenames ? JSON.parse(bid.products.filenames).map(image => `https://secondhand-backend-kita.herokuapp.com/images/products/${image}`)[0] : "";
+
+        if (bid.acceptedAt) {
+          notif.push({
+            id: 0,
+            productName: bid.products.name,
+            price: bid.products.price,
+            image: images,
+            type: "Penawaran Produk",
+            bidPrice: bid.bidPrice,
+            time: bid.acceptedAt
+          });
+        }
+
+        if (bid.declinedAt) {
+          notif.push({
+            id: 0,
+            productName: bid.products.name,
+            price: bid.products.price,
+            image: images,
+            type: "Penawaran ditolak",
+            bidPrice: bid.bidPrice,
+            time: bid.declinedAt
+          })
+        }
+
+      });
+
+      notif.sort((a, b) => new Date(b.time).getTime() - new Date(a.time).getTime());
+      notif.map(rows => rows.id = id++);
+
+      if (notif.length > 10) {
+        notif = notif.slice(0, 10);
+      }
+
+      res.status(200).json({
+        data: notif,
+        // asd: myProducts.rows[2].bids.bidPrice ? true : false
+      });
+
+    } catch (err) {
+      return res.status(403).json({
+        message: ``,
+        errors: err.message
+      })
+    }
   }
 
 }
