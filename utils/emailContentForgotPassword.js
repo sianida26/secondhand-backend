15db8961a10a084d48081e6bc008f589c9eec6ce
naufalafif76 <naufalafif766@gmail.com>
2022-07-08 21:27:08 +0700

    [edit] emailContentForgotPassword.js

--- utils/emailContentForgotPassword.js
+++ utils/emailContentForgotPassword.js
@@ -1,16 +1,16 @@
 const emailContentForgotPassword = (token) => {
   return `
-    Hallo,
-    <br />
-    Email ini dikirimkan kepadamu karena kamu meminta untuk melakukan pengaturan ulang kata sandi dari SecondHand
-    <br />
-    <br />
-    Untuk mengatur ulang password kamu, klik link berikut:
-    <br />
-    https://secondhand-kita.vercel.app/forgot-password/${token}
-    <br />
-    <br />
-    SecondHand Customer Service
+    <body>
+      Email ini dikirim karena kamu meminta untuk melakukan pengaturan ulang kata sandi dari SecondHand
+      <br />
+      <br />
+      Untuk mengatur ulang password kamu, klik link berikut:
+      <br />
+      https://secondhand-kita.vercel.app/forgot-password/${token}
+      <br />
+      <br />
+      SecondHand Customer Service
+    </body>
   `;
 };
 
