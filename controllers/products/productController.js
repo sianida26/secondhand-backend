aafbeb0c0cbf09c024eff12f9cb086d62934f3f3
naufalafif76 <naufalafif766@gmail.com>
2022-07-13 05:26:02 +0700

    [edit] prodcutController.js - fix return

--- controllers/products/productController.js
+++ controllers/products/productController.js
@@ -8,12 +8,12 @@ module.exports = {
   async handleGetAllProducts(req, res) {
     try {
       const products = await Products.findAndCountAll({ include: ['users'] });
-      res.status(200).json({
+      return res.status(200).json({
         data: products.rows,
         count: products.count,
       });
     } catch (err) {
-      res.status(400).json({
+      return res.status(400).json({
         message: 'Failed',
         errors: err.message,
       });
@@ -26,7 +26,7 @@ module.exports = {
       const profilePhoto = productId.users.image ? `https://secondhand-backend-kita.herokuapp.com/images/profile/${productId.users.image}` 
         : `https://avatars.dicebear.com/api/bottts/${productId.users.id}.svg`;
 
-      res.status(200).json({
+      return res.status(200).json({
         id: productId.id,
         name: productId.name,
         images: productId.filenames ? JSON.parse(productId.filenames).map((image) => `https://secondhand-backend-kita.herokuapp.com/images/products/${image}`) : [],
@@ -40,7 +40,7 @@ module.exports = {
         price: productId.price,
       });
     } catch (err) {
-      res.status(404).json({
+      return res.status(404).json({
         message: `Product with id ${req.params.id} not found`,
         errors: err.message,
       });
@@ -78,7 +78,7 @@ module.exports = {
         if (!isBiddable) return res.status(404).json({ message: `Produk dengan id ${ req.params.id } tidak tersedia.` });
       }
 
-      res.status(200).json({
+      return res.status(200).json({
         id: product.id,
         name: product.name,
         images: product.imageUrls,
@@ -93,7 +93,7 @@ module.exports = {
         status: bidStatus,
       });
     } catch (err) {
-      res.status(500).json({
+      return res.status(500).json({
         message: `Terjadi kesalahan pada server`,
         errors: err.message,
       });
@@ -122,7 +122,7 @@ module.exports = {
 
       myProducts.rows.sort((a, b) => new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime());
       myProducts.rows.map((product) => {
-        let images = product.filenames ? JSON.parse(product.filenames).map((image) => `https://secondhand-backend-kita.herokuapp.com/images/products/${image}`)[0] : '';
+        let images = product.filenames ? product.imageUrls[0] : '';
 
         products.push({
           id: product.id,
@@ -163,14 +163,14 @@ module.exports = {
           });
         }
       });
-      res.status(200).json({
+      return res.status(200).json({
         products: products,
         diminati: interestedProducts,
         terjual: soldProducts,
         count: myProducts.count,
       });
     } catch (err) {
-      res.status(400).json({
+      return res.status(400).json({
         message: 'Failed',
         errors: err.message,
       });
